b63d7d3d02f1309a493a37675f34938b3b80a84d
FMMACubos <113648495+FMMACubos@users.noreply.github.com>
2022-12-20 12:09:19 +0000

    Update main.dart

--- desafio_1/lib/main.dart
+++ desafio_1/lib/main.dart
@@ -1,8 +1,5 @@
 import 'package:desafio_1/telas/pag2.dart';
 import 'package:flutter/material.dart';
-import 'package:desafio_1/modelos/variaveis.dart';
-import 'package:desafio_1/modelos/dado.dart';
-import 'package:desafio_1/modelos/lista_dados.dart' as lista_dados;
 import 'package:desafio_1/telas/pag1.dart';
 
 final pageController = PageController();
@@ -12,14 +9,10 @@ void main() {
     debugShowCheckedModeBanner: false,
     home: PageView(
       controller: pageController,
-      children:  [
-      ResponsividadeMediaQuery(), 
-      ResponsividadeMediaQuery2(), 
-      
-      
-    ],
-  ),
-  )
-  );
+      children: [
+        ResponsividadeMediaQuery(),
+        ResponsividadeMediaQuery2(),
+      ],
+    ),
+  ));
 }
-
